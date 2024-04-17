e1008cf4e2e98e4e4d479820d7f96331bedbffdb
Ramadan <daudalaramadan2@gmail.com>
2024-04-17 10:32:05 +0000

    p1

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // declare and initialize an object
         javaCU p1 = new javaCU();
+        // this creates an object and stores in inside the p1
         p1.name = "pikachu";
         p1.number = 23;
         System.out.println(p1.name + " " + p1.number);
--- javaCU.java
+++ javaCU.java
@@ -4,7 +4,9 @@ public class javaCU {
     int number;
 
     // write a method that doesnt return anything
-    // this class has also been defined so let declare an obj
+    // this class has also been defined so let declare an
+    // obj inside the main class
+    // this class will be a blue print for creating objects
     void attack() {
         System.err.println(name + "attack");
     }
