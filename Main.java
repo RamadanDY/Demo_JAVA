2f8ce99f912e06c20b59bc2ecdcebea1ff487525
Ramadan <daudalaramadan2@gmail.com>
2024-04-17 10:20:10 +0000

    running

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ public class Main {
         javaCU p1 = new javaCU();
         p1.name = "pikachu";
         p1.number = 23;
-
         System.out.println(p1.name + " " + p1.number);
 
+        javaCU p2 = new javaCU();
+        p2.name = "yakubu";
+        System.out.println(p2.name + " " + p1.number);
+
+        p2.attack();
     }
 }
