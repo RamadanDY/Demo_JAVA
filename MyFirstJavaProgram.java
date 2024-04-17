45bb7a6388955e528e9c5a33f7f43035afb44b49
Ramadan <daudalaramadan2@gmail.com>
2024-04-17 08:56:25 +0000

    My first java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,10 +1,48 @@
 public class MyFirstJavaProgram {
+   // Instance variable num
+   int num = 10;
 
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
+   MyFirstJavaProgram() {
+      System.out.println("This is an example program on keyword this");
+   }
 
-   public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+   MyFirstJavaProgram(int num) {
+      // Invoking the default constructor
+      this();
+
+      // Assigning the local variable <i>num</i> to the instance variable <i>num</i>
+      this.num = num;
+   }
+
+   public void greet() {
+      System.out.println("Hi Welcome to Tutorialspoint");
+   }
+
+   public void print() {
+      // Local variable num
+      int num = 20;
+
+      // Printing the local variable
+      System.out.println("value of local variable num is : " + num);
+
+      // Printing the instance variable
+      System.out.println("value of instance variable num is : " + this.num);
+
+      // Invoking the greet method of a class
+      this.greet();
+   }
+
+   public static void main(String[] args) {
+      // Instantiating the class
+      MyFirstJavaProgram obj1 = new MyFirstJavaProgram();
+
+      // Invoking the print method
+      obj1.print();
+
+      // Passing a new value to the num variable through parametrized constructor
+      MyFirstJavaProgram obj2 = new MyFirstJavaProgram(30);
+
+      // Invoking the print method again
+      obj2.print();
    }
 }
