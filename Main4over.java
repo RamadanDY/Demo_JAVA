7cc182075bb241494c481ffe241fa08a22fc159d
Ramadan <daudalaramadan2@gmail.com>
2024-04-22 19:59:20 +0000

    addition of

--- Main4over.java
+++ Main4over.java
@@ -1,5 +1,24 @@
 public class Main4over {
     public static void main(String[] args) {
+        int result = add(3, 4);
+        System.out.println("result : " + result);
 
+    }
+
+    static int add(int a, int b) {
+        System.out.println("this is overloaded method number one ");
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        System.out.println("this is overloaded method number two ");
+
+        return a + b + c;
+    }
+
+    static int add(int a, int b, int c, int d) {
+        System.out.println("this is overloaded method number three ");
+
+        return a + b + c + d;
     }
 }
