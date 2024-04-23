b0c5dca6542e2cfc7fdccc1eee96f168032b3052
Ramadan <daudalaramadan2@gmail.com>
2024-04-23 12:17:51 +0000

    a test

--- Main4over.java
+++ Main4over.java
@@ -1,7 +1,9 @@
 public class Main4over {
     public static void main(String[] args) {
-        int result = add(3, 4);
-        System.out.println("result : " + result);
+        // int result = add(3, 4);
+        // System.out.println("result : " + result);
+        int abeg = add(1, 2);
+        System.out.println(abeg);
 
     }
 
