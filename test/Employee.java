fe8a45eedbca171461ff568946eaabaea3c27837
Ramadan <daudalaramadan2@gmail.com>
2024-04-23 13:48:05 +0000

    getting data from users

--- test/Employee.java
+++ test/Employee.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.Scanner;
+// u need to import scanner fpor u to be able to recieve imports from user 
+
 public class Employee {
     int empno;
     String ename;
@@ -19,6 +22,14 @@ public class Employee {
 
     public float havedata() {
         return empno * (hr + da);
+    }
+
+    public void enterData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Employee Number:");
+        empno = scanner.nextInt();
+        scanner.close();
+
     }
 
     public static void main(String[] args) {
